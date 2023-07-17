ee5744b51f84802dab54753002599bb83e35e2a6
gerald <gerald.mahlknecht@veios.com>
2023-07-17 10:16:15 +0200

    add modal to todo list item task

--- frontend/src/pages/tasks/react-tasks/TodoList.tsx
+++ frontend/src/pages/tasks/react-tasks/TodoList.tsx
@@ -1,8 +1,8 @@
-import React, { FC, useState } from "react";
+import React, { FC, useCallback, useState } from "react";
 import { Task } from "../../../index";
 import { TaskWrapper } from "../../../components/TaskWrapper";
 import uuid from "react-uuid";
-import { Button, Checkbox, notification } from "antd";
+import { Button, Checkbox, Input, Modal, notification } from "antd";
 import { DeleteTwoTone, PlusOutlined } from "@ant-design/icons";
 
 interface TodoItemProps {
@@ -40,10 +40,20 @@ export const TodoList: FC<Task> = (task) => {
     },
   ];
 
+  const [newItem, setNewItem] = useState<TodoItemProps | null>(null);
+
   const [todoItems] = useState<TodoItemProps[]>(initialItems);
 
   // TODO: Remove the dummy handler before submitting your solution
-  const dummyHandler = () => notification.error({ message: "TODO" });
+  const openModal = () => {
+    notification.error({ message: "TODO open modal" });
+  };
+
+  const handleAdd = (name: string) => {
+    notification.error({
+      message: `TODO add item ${name} to list and close modal`,
+    });
+  };
 
   const TodoItem: FC<TodoItemProps> = ({ done, name }) => {
     return (
@@ -54,11 +64,11 @@ export const TodoList: FC<Task> = (task) => {
       >
         {name}
         <div className={"flex justify-content items-center space-x-4"}>
-          <Checkbox checked={done} onChange={dummyHandler} />
+          <Checkbox checked={done} onChange={openModal} />
           <DeleteTwoTone
             className={"cursor-pointer"}
             twoToneColor={"red"}
-            onClick={dummyHandler}
+            onClick={openModal}
           />
         </div>
       </div>
@@ -75,15 +85,32 @@ export const TodoList: FC<Task> = (task) => {
         </div>
         <Button
           block
-          onClick={dummyHandler}
+          onClick={openModal}
           icon={<PlusOutlined />}
           type={"dashed"}
           className={"mt-4"}
         >
           Add item
         </Button>
+        <ToDoItemInput open={!!newItem} onCreate={handleAdd} />
       </div>
     </TaskWrapper>
+  );
+};
+
+const ToDoItemInput: FC<{
+  open: boolean;
+  onCreate: (name: string) => void;
+}> = ({ onCreate, open }) => {
+  // The todo item name must not be empty
+  return (
+    <Modal
+      title={"New Item"}
+      open={open}
+      footer={<Button onClick={() => onCreate("")}>Create</Button>}
+    >
+      <Input />
+    </Modal>
   );
 };
 
